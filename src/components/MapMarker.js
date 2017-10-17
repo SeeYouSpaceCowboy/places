a905fbe9a53bb95feecf4c1451bfd55191235c3a
Mohammed Chisti <93mchisti@gmail.com>
2017-10-17 11:20:12 -0400

    Updates

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -2,7 +2,7 @@ import React from 'react'
 import markerImg from '../stylesheets/map-pin-b.png'
 
 const MapMarker = (props) => {
-  return <img src={ markerImg } alt='marker' />
+  return <img src={ markerImg } alt='marker' style={ { width: '4em', height: '4em' } }/>
 }
 
 export default MapMarker
